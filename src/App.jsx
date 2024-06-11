3c5a197c58ce5b7cb5f591820562975bd09b94b1
mohamed <maxamedcabdifitaax33@gmail.com>
2024-06-11 22:08:58 +0300

    added more files and Router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import Header from "./Components/Header";
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Layout from "./Components/Layout";
+import Home from "./Pages/Home"
+import Product from "./Pages/Product";
 function App() {
   return (
     <>
-      <Header />
+     <Router>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route index element={<Home/>} />
+          <Route path="/product" element={<Product/>}/>
+        </Route>
+      </Routes>
+     </Router>
     </>
   );
 }
