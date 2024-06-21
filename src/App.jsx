86712f510ef199f3808775eac5a9dbd8b9333598
mohamed <maxamedcabdifitaax33@gmail.com>
2024-06-21 20:16:51 +0300

    idk

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Home from "./Pages/Home";
 import Layout from "./Components/Layout";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Product from "./Pages/Product";
+import Store from "./Pages/Store";
 function App() {
   return (
     <>
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
+            <Route path="/store" element={<Store />} />
             <Route path="/product/:id" element={<Product />} />
           </Route>
         </Routes>
--- src/Pages/Store.jsx
+++ src/Pages/Store.jsx
@@ -1,7 +1,12 @@
 
 const Store = () => {
   return (
-    <div>Store</div>
+   <>
+    <div className="p-4 flex justify-between">
+      <div className="w-4/12 bg-grayishBlue p-4"></div>
+      <div className="w-7/12 bg-grayishBlue p-4"></div>
+    </div>
+   </>
   )
 }
 
